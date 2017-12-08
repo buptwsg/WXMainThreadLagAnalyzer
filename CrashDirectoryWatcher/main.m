f59db9a4a04d18005e4808fa8ae95ac4e3b00ea9
sulirong <15528290768@163.com>
2017-12-08 14:29:24 +0800

    add dir watcher class

---
+++ CrashDirectoryWatcher/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CrashDirectoryWatcher
+//
+//  Created by sulirong on 2017/12/8.
+//  Copyright © 2017年 buptwsg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
